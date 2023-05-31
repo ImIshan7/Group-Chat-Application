5335846f57153f1e51aebdc16095f81d4cfe2f7a
ImIshan7 <ishandanu999@gmail.com>
2023-05-31 08:53:12 +0100

    Complete UI design

---
+++ src/lk/ijse/chatapplication/controller/ClientFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.chatapplication.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientFormController {
+
+    @FXML
+    private Label lblClientName;
+
+    @FXML
+    private AnchorPane sendMsg;
+
+    @FXML
+    void ImageMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void msgSendOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/lk/ijse/chatapplication/controller/ClientLoginFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.chatapplication.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientLoginFormController {
+
+    @FXML
+    private AnchorPane Login;
+
+    @FXML
+    private Button btnlogin;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnloginOnAction(ActionEvent event) {
+
+    }
+
+}
