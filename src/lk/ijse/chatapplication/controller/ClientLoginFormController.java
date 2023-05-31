e1086494678dd7c8ea6d1d93989a9231157bcc35
ImIshan7 <ishandanu999@gmail.com>
2023-05-31 10:15:51 +0100

    Complete Some Part In Project

--- src/lk/ijse/chatapplication/controller/ClientLoginFormController.java
+++ src/lk/ijse/chatapplication/controller/ClientLoginFormController.java
@@ -2,12 +2,16 @@ package lk.ijse.chatapplication.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ClientLoginFormController {
-
     @FXML
     private AnchorPane Login;
 
@@ -17,8 +21,17 @@ public class ClientLoginFormController {
     @FXML
     private TextField txtUserName;
 
+    static String username;
+
     @FXML
-    void btnloginOnAction(ActionEvent event) {
+    void btnloginOnAction(ActionEvent event) throws IOException {
+
+        username=txtUserName.getText();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(ClientLoginFormController.class.getResource("../view/ClientForm.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
